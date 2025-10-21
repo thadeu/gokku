01365ba356011be8dc6955fa8be0cecfe57e1e43
thadeu <tadeuu@gmail.com>
2025-10-21 14:28:03 -0300

    docs: update version to 1.0.39

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     logo: '/logo.svg',
 
     nav: [
-      { text: '1.0.37', link: '/' },
+      { text: '1.0.39', link: '/' },
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/getting-started' },
       { text: 'Examples', link: '/examples/' },
