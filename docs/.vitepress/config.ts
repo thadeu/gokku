87d7727b93999559362601b1fff8ba0430c441a8
thadeu <tadeuu@gmail.com>
2025-10-23 15:01:04 -0300

    docs: update version to 1.0.63

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -15,7 +15,7 @@ export default withMermaid(defineConfig({
     logo: '/logo.svg',
 
     nav: [
-      { text: '1.0.62', link: '/' },
+      { text: '1.0.63', link: '/' },
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/getting-started' },
       { text: 'Examples', link: '/examples/' },
